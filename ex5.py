fd61dab413335f7ee513844bcfae7650ebfd028f
jeff-cangialosi <jeffcang19@gmail.com>
2020-09-15 14:50:17 -0400

    Redid Examples 5 and 6

--- ex5.py
+++ ex5.py
@@ -1,23 +1,26 @@
 # Exercise 5
 # This was coded while listening to the Hamilton soundtrack
 
-name = 'Jeff Cangialosi'
-age = 29 # this is now a high number
-height = 70 # inches
-weight = 170 # lbs
-height_cm = round(height * 2.54, 0)
-weight_kg = round(weight * 0.453592, 3)
-eyes = 'blue'
-teeth = 'white'
-hair = 'brown'
+mb_name = 'Giannis'
+mb_age = 25 # I think this is correct
+mb_height = 84 # Also a guess but probably close
+mb_weight = 260 # Hopefully close
+mb_eyes = 'Brown'
+mb_teeth = 'White'
+mb_hair = 'Black'
 
-print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall. If we convert that to centimeters, that's {height_cm} centimeters.")
-print(f"He's {weight} pounds heavy. If we convert that to kilograms, that's {weight_kg} kilograms.")
+print(f"Let's talk about {mb_name}.")
+print(f"He's {mb_height} inches tall.")
+print(f"He's {mb_weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {eyes} eyes and {hair} hair.")
-print(f"His teeth are usually {teeth} depending on the coffee.")
+print(f"He's got {mb_eyes} eyes and {mb_hair} hair.")
+print(f"His teeth are usually {mb_teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = age + height + weight
-print(f"If I add {age}, {height}, and {weight} I get {total}.")
+total = mb_age + mb_height + mb_weight
+print(f"If I add {mb_age}, {mb_height}, and {mb_weight} I get {total}.")
+
+centi_converter = 2.54
+
+mb_height_cm = mb_height * centi_converter
+print(f"Converting the height into centimeters, we get {mb_height_cm}.")
--- ex6.py
+++ ex6.py
@@ -1,38 +1,31 @@
 # Exercise 6
 # This was written listening to the Hamilton soundtrack
 
-# Creating a variable and assigning a numeric to it
+# Creating some variables with a numeric and then a string
 types_of_people = 10
-
-# Variable x with an f-string to be able to add a variable in
 x = f"There are {types_of_people} types of people."
 
-# Basic variables here assigned to text strings
+# Creating 3 variables with the last value including an f string
 binary = "binary"
 do_not = "don't"
-
-# Have a varibale with another f-string here with two variables implanted
 y = f"Those who know {binary} and those who {do_not}."
 
-# Printing the variables x and y here to show their outputs
 print(x)
 print(y)
 
-# Printing f-strings here with x and y embedded
+# Printing f strings using the newly created variables x and y
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
-# Assigning the Boolean value of False to a variable named hilarious
+# A variable with a boolean value
 hilarious = False
 
-# Created a string here and left an open bracket to allowing it to be formatted
 joke_evaluation = "Isn't that joke so funny?! {}"
 
-# Using a .format syntax here - so fancy!!
+# Using .format here so that we can add something to a previously created string
 print(joke_evaluation.format(hilarious))
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
-# Combining variables with 2 text strings here
 print(w + e)
